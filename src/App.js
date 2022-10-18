d6e845dbe8b2dcccaa8fb446cc1c5944e2cfa5ac
Cody Marshall <codymstuntsx@gmail.com>
2022-10-18 16:01:26 -0700

    added dependencies and passport starter code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-     Hey Team 😇
-     Hey Team Again 😇
+      <Header />
     </div>
   );
 }
