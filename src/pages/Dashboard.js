3fd592096d78a8729db4dc988d1a74a0c6e5e7c7
Elliot Erickson <eric0952@gmail.com>
2022-10-19 12:32:59 -0700

    Centered text
    Centered dummy text for readablility, even though it will be removed

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -40,10 +40,10 @@ const Dashboard = () => {
                 <div className='dashboard_tracker_container row-span-3 flex flex-column'>
                     {trackerItems.map((track) => (
                         <div 
-                            className='dashboard_tracker basis-2/6 border-2 m-2' 
+                            className='dashboard_tracker basis-2/6 border-2 m-2 rounded-3xl' 
                             key={nanoid()}
                         >
-                            <p className='dashboard_tracker_text'>{track.item}</p>
+                            <p className='dashboard_tracker_text  text-center'>{track.item}</p>
                         </div> 
                     ))}
                 </div>
@@ -55,11 +55,11 @@ const Dashboard = () => {
                 <div className='dashboard_job-card_container row-span-3 flex flex-column'>
                     {jobCards.map((job) => (
                         <div 
-                            className='dashboard_job-card_item basis-2/6 border-2 m-4' 
+                            className='dashboard_job-card_item basis-2/6 border-2 m-4 rounded-3xl' 
                             key={nanoid()}
                         >
-                            <p className='dashboard_job-card_text'>{job.item}</p>
-                        </div>  
+                            <p className='dashboard_job-card_text text-center'>{job.item}</p>
+                        </div> 
                     ))}
                 </div>
             </div>
@@ -67,7 +67,7 @@ const Dashboard = () => {
                 <p className='dashboard_job-tracker_text self-end ml-8 text-xl'>
                     Weekly Job Application Tracker
                 </p>
-                <div className='dashboard_job-tracker_graph border-2 row-span-3 m-6'>
+                <div className='dashboard_job-tracker_graph border-2 row-span-3 m-6 rounded-3xl  text-center'>
                     Graph Goes Here
                 </div>
             </div>
