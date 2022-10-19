511d9a6d09daa68d80cd86007146c569be75dbf0
Cody Marshall <codymstuntsx@gmail.com>
2022-10-18 20:18:46 -0700

    tailwind styles added

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -7,6 +7,7 @@ function LoginModal({ setShowModalLogin }) {
       id="authentication-modal"
       tabIndex="-1"
       aria-hidden="true"
+      className="justify-center flex-col items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-gray-600 bg-opacity-50"
     >
       <Login />
       <button onClick={() => setShowModalLogin(false)}>Close</button>
