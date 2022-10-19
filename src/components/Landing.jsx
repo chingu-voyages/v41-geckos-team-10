767a5c06ede09196cb6a5fc62f1b6f966c11da76
Cody Marshall <codymstuntsx@gmail.com>
2022-10-18 19:26:58 -0700

    created state to show login signout modal

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,25 +1,50 @@
-import './Landing.css';
+import { useState } from "react";
+import "./Landing.css";
+import LoginModal from "./LoginModal";
+import SignupModal from "./SignupModal";
 
-function Landing () {
-    return (
-        <div className="landing landing--div">
-            <div>
-                <h1 className="landing landing--h1">Job Hunt Tracker</h1>
+function Landing() {
+  const [showModalSignup, setShowModalSignup] = useState(false);
+  const [showModalLogin, setShowModalLogin] = useState(false);
 
-                <p className="landing landing--p">The rewarding way to track your job applications.</p>
-            </div>
-            
-            <div>
-                <button className="button button--landing">
-                Log in
-                </button>
+  const handleClickLogin = () => {
+    setShowModalLogin(true);
+  };
 
-                <button className="button button--landing">
-                Sign up
-                </button>
-            </div> 
-        </div>
-    )
+  const handleClickSignup = () => {
+    setShowModalSignup(true);
+  };
+
+  return (
+    <div className="landing landing--div">
+      <div>
+        <h1 className="landing landing--h1">Job Hunt Tracker</h1>
+
+        <p className="landing landing--p">
+          The rewarding way to track your job applications.
+        </p>
+      </div>
+
+      <div>
+        <button
+          className="button button--landing"
+          onClick={() => handleClickLogin()}
+          data-modal-toggle="authentication-modal"
+        >
+          Log in
+        </button>
+        <button
+          className="button button--landing"
+          onClick={() => handleClickSignup()}
+          data-modal-toggle="authentication-modal"
+        >
+          Sign up
+        </button>
+      </div>
+      {showModalLogin && <LoginModal setShowModalLogin={setShowModalLogin} />}
+      {showModalSignup && <SignupModal setShowModal={setShowModalSignup} />}
+    </div>
+  );
 }
 
 export default Landing;
