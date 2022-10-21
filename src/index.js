c8b1c0a9e99c79df432ed2d87c7543dd21a0928b
Kristi <kristiana.d.ingco@gmail.com>
2022-10-20 19:32:35 -0700

    Add profile page to app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-
-import React from 'react';
-import  ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Dashboard from './pages/Dashboard';
-import './index.css';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Dashboard from "./pages/Dashboard";
+import "./index.css";
+import ProfilePage from "./components/ProfilePage/ProfilePage";
+import Landing from "./components/Landing";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />} >
-          <Route path='/Dashboard' element={<Dashboard />} />
+        <Route path="/" element={<App />}>
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/profile-page" element={<ProfilePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
