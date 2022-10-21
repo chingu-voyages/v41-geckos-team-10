1db47859f65abe15172abbeb10b88a9642de1756
Kristi <kristiana.d.ingco@gmail.com>
2022-10-20 19:40:20 -0700

    Add state to ProfileTextInput

--- src/components/ProfilePage/Profile Form Inputs/ProfileTextInput/ProfileTextInput.jsx
+++ src/components/ProfilePage/Profile Form Inputs/ProfileTextInput/ProfileTextInput.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import "./ProfileTextInput.css";
 
-const ProfileTextInput = ({ name, type = "text" }) => {
+const ProfileTextInput = ({ name, value = "", type = "text" }) => {
+  const [inputValue, setInputValue] = useState(value);
+
   const labelText = (label) => {
     const parsedText = label.split("-").join(" ");
     return parsedText.charAt(0).toUpperCase() + parsedText.slice(1);
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
   };
 
   return (
@@ -12,6 +19,8 @@ const ProfileTextInput = ({ name, type = "text" }) => {
       <input
         type={type}
         name={name}
+        value={inputValue}
+        onChange={handleChange}
         className={`profile-text-input__textbox`}
       />
     </div>
