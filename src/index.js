51ce92f5de7f7c0a49682ca45a2ee2c4302f5580
Cody Marshall <codymstuntsx@gmail.com>
2022-10-22 15:08:33 -0700

    added logingform route

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-
-import React from 'react';
-import  ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Dashboard from './pages/Dashboard';
-import './index.css';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import LoginForm from "./components/LoginForm";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />} >
-          <Route path='/Dashboard' element={<Dashboard />} />
+        <Route path="/" element={<App />}>
+          <Route path="/Dashboard" element={<Dashboard />} />
         </Route>
+        <Route path="/loginform" element={<LoginForm />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
