e1fc171f0c9350186f0892ed238a515bca3bde9b
Cody Marshall <codymstuntsx@gmail.com>
2022-10-21 17:45:26 -0700

    refactored code from previous files

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Form.css";
+
+function LoginForm() {
+  const [errMsg, setErrMsg] = useState("");
+
+  const loginUser = async (e) => {
+    setErrMsg("");
+
+    e.preventDefault();
+    const body = {
+      username: e.currentTarget.username.value,
+      password: e.currentTarget.password.value,
+    };
+
+    //axios post call to login and if successful, redirect to home
+    axios
+      .post("/login", body)
+      .then((response) => {
+       if( response.status === 200 )
+       setErrMsg(response.data);
+      })
+      .catch((error) => {
+        console.error(error.response.data);
+        setErrMsg("Invalid username or password");
+      });
+  };
+
+  return (
+    <form onSubmit={(e) => loginUser(e)} className="form-container">
+      <label>
+        <span className="form--span">Email</span>
+        <input type="text" name="username" className="form--input" required />
+      </label>
+      <label>
+        <span className="form--span">Password</span>
+        <input
+          type="password"
+          name="password"
+          className="form--input"
+          required
+        />
+      </label>
+
+      <div className="submit">
+        <button type="submit" className="form--button">
+          Log in
+        </button>
+      </div>
+      {errMsg && <span className="error">{errMsg}</span>}
+    </form>
+  );
+}
+
+export default LoginForm;
---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import axios from "axios";
+import "./Form.css";
+
+function RegisterForm() {
+  const registerUser = async (e) => {
+    e.preventDefault();
+    const body = {
+      username: e.currentTarget.username.value,
+      password: e.currentTarget.password.value,
+    };
+    console.log(e);
+
+    //axios post call to register and if successful, redirect to login
+    axios
+      .post("/register", body)
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          console.log("User registered");
+        }
+      })
+      .catch((error) => {
+        console.error(error.response.data);
+      });
+  };
+
+  return (
+    <form onSubmit={(e) => registerUser(e)} className="form-container">
+      <label>
+        <span className="form--span">Email</span>
+        <input type="text" name="username" className="form--input" required />
+      </label>
+      <label>
+        <span className="form--span">Password</span>
+        <input
+          type="password"
+          name="password"
+          className="form--input"
+          required
+        />
+      </label>
+      <label>
+        <span className="form--span">Repeat password</span>
+        <input
+          type="password"
+          name="rpassword"
+          className="form--input"
+          required
+        />
+      </label>
+
+      <div className="submit">
+        <button type="submit" className="form--button">
+          Sign up
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default RegisterForm;
