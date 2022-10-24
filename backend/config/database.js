7d5f0127395be695c63fb7fd924ef855f7bedf8f
Cody Marshall <codymstuntsx@gmail.com>
2022-10-24 08:56:38 -0700

    moved user model to own file

--- backend/config/database.js
+++ backend/config/database.js
@@ -10,16 +10,3 @@ mongoose
   )
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
-
-//create a schema for  users
-const userSchema = new mongoose.Schema({
-  username: String,
-  hash: String,
-  salt: String,
-});
-
-//create a model for users
-const User = mongoose.model("User", userSchema);
-
-//export the model
-module.exports = User;
