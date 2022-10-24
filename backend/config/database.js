a06d160948cded9084f7f4b22ade7273faadeb7f
Cody Marshall <codymstuntsx@gmail.com>
2022-10-24 10:11:52 -0700

    created new function for connecting database

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,11 +2,16 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-// Connect to MongoDB
-mongoose
-  .connect(
-    "mongodb+srv://cody:testchingu@cluster0.ssijp8i.mongodb.net/jobtracker?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb+srv://cody:testchingu@cluster0.ssijp8i.mongodb.net/jobtracker?retryWrites=true&w=majority", {
+      useNewUrlParser: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDb;
