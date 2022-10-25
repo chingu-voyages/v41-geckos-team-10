87bc605718f48b7dfcf6309aede54e9850d1bab4
MoxElliot <eric0952@gmail.com>
2022-10-25 09:37:19 -0700

    NavBar Styling
    Reverted NavBar stylig back to plain css and commented out the tailwind until it can be cleaned up

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,19 +16,19 @@ const NavBar = ()=> {
                     <img className="logo" alt="app logo"/>
                 </li>
                 <li className="navbar navbar--profile_picture">
-                    <img src={profile_picture_holder} className="navbar navbar--profile_picture" alt="profile"/>
+                    <img src={profile_picture_holder} className="" alt="profile"/>
                 </li>
                 <li className="navbar navbar--list_item">
                     <img src={dashboard_icon} className="navbar navbar--icon" alt="dashboard page"/>
                     <Link to='/Dashboard' className="navbar navbar--text">Dashboard</Link>
                 </li>
                 <li className="navbar navbar--list_item">
-                    <img src={tracker_icon} className="navbar navbar--icon" alt="tracker page"/>
-                    <Link to='/Tracker' className="navbar navbar--text">Tracker</Link>
-                </li>
-                <li className="navbar navbar--list_item">
                     <img src={profile_icon} className="navbar navbar--icon" alt="profile page"/>
                     <Link to='/Profile' className="navbar navbar--text">Profile</Link>
+                </li>
+                <li className="navbar navbar--list_item">
+                    <img src={tracker_icon} className="navbar navbar--icon" alt="tracker page"/>
+                    <Link to='/Tracker' className="navbar navbar--text">Tracker</Link>
                 </li>
                 <li className="navbar--button--div">
                     <button className="navbar navbar--button">Add Task +</button>
