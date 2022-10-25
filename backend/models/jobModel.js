e3ec03ff06240e5120fc0e721552a0c3c598548d
Cody Marshall <codymstuntsx@gmail.com>
2022-10-25 12:31:10 -0700

    created job model

--- backend/models/jobModel.js
+++ backend/models/jobModel.js
@@ -1,25 +1,52 @@
 //create a job schema
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const jobSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    location: {
-        type: String,
-        required: true,
-    },
-    datePosted: {
-        type: Date,
-        required: true,
-    },
+  title: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  dateApplied: {
+    type: Date,
+  },
+  followUp: {
+    type: Boolean,
+    required: true,
+  },
+  jobLink: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  resumeSent: {
+    type: Boolean, //<=======Not sure of the value type
+    required: true,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const Job = mongoose.model('Job', jobSchema);
+const Job = mongoose.model("Job", jobSchema);
 
 module.exports = Job;
