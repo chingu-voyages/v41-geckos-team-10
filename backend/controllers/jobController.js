7334dd392e107462d3ba16356361946fc127c211
Cody Marshall <codymstuntsx@gmail.com>
2022-10-28 08:51:43 -0700

    destructured req body

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -16,21 +16,49 @@ const getJobs = async (req, res) => {
 //@route GET /jobs:id
 //@access Private
 const getJobById = async (req, res) => {
-    try {
-        const job = await Job.findById(req.params.id);
-        res.status(200).send(job);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-}
+  try {
+    const job = await Job.findById(req.params.id);
+    res.status(200).send(job);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 //@desc Create a job
 //@route POST /jobs
 //@access Private
 const createJob = async (req, res) => {
+  const {
+    title,
+    company,
+    status,
+    dateApplied,
+    followUp,
+    jobLink,
+    location,
+    contact,
+    resumeSent,
+    salary,
+    timestamp,
+  } = req.body;
+
   try {
-    const job = await Job.create(req.body);
-    res.status(200).send(job);
+    const newJob = new Job({
+      title,
+      company,
+      status,
+      dateApplied,
+      followUp,
+      jobLink,
+      location,
+      contact,
+      resumeSent,
+      salary,
+      timestamp,
+    });
+
+    const job = await newJob.save();
+    res.status(201).send(job);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -44,11 +72,11 @@ const updateJob = async (req, res) => {
     const job = await Job.findById(req.params.id);
     if (!job) {
       res.status(400).json({ message: "Job not found" });
-    } 
+    }
     const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
+      new: true,
     });
-      res.status(200).send(`Job ${updatedJob.title} has been updated`);
+    res.status(200).send(`Job ${updatedJob.title} has been updated`);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -61,7 +89,7 @@ const deleteJob = async (req, res) => {
   try {
     const job = await Job.findById(req.params.id);
     if (!job) {
-        res.status(400).json({ message: "Job not found" });
+      res.status(400).json({ message: "Job not found" });
     }
     await Job.findByIdAndDelete(req.params.id);
     res.status(200).send(`Job ${job.title} has been deleted`);
