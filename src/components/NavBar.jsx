59c4cbd85727a710548e51820afd8693590ebdc7
Cody Marshall <codymstuntsx@gmail.com>
2022-10-28 08:54:11 -0700

    call to logout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,46 +1,85 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import "./NavBar.css";
-import dashboard_icon from "../assets/dashboardicon.svg"
+import dashboard_icon from "../assets/dashboardicon.svg";
 import tracker_icon from "../assets/trackericon.svg";
 import profile_icon from "../assets/profileicon.svg";
 import profile_picture_holder from "../assets/dwight.jpg";
-import logout_icon from "../assets/logouticon.svg"
+import logout_icon from "../assets/logouticon.svg";
 
+const NavBar = () => {
+  const navigate = useNavigate();
 
-const NavBar = ()=> {
- return (
+  const handleLogout = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/logout");
+      if (res.status === 200) {
+        alert(res.data);
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
     <div className="navbar navbar--div">
-        <nav className="navbar navbar--nav">
-            <ul className="navbar navbar--list">
-                <li className="navbar navbar--logo">
-                    <img className="logo" alt="app logo"/>
-                </li>
-                <li className="navbar navbar--profile_picture">
-                    <img src={profile_picture_holder} className="" alt="profile"/>
-                </li>
-                <li className="navbar navbar--list_item">
-                    <img src={dashboard_icon} className="navbar navbar--icon" alt="dashboard page"/>
-                    <Link to='/Dashboard' className="navbar navbar--text">Dashboard</Link>
-                </li>
-                <li className="navbar navbar--list_item">
-                    <img src={profile_icon} className="navbar navbar--icon" alt="profile page"/>
-                    <Link to='/Profile' className="navbar navbar--text">Profile</Link>
-                </li>
-                <li className="navbar navbar--list_item">
-                    <img src={tracker_icon} className="navbar navbar--icon" alt="tracker page"/>
-                    <Link to='/Tracker' className="navbar navbar--text">Tracker</Link>
-                </li>
-                <li className="navbar--button--div">
-                    <button className="navbar navbar--button">Add Task +</button>
-                </li>
-                <li className="navbar navbar--logout">
-                    <img src={logout_icon} className= "navbar--icon_logout" alt="logout"/>
-                    <Link className="navbar--text_logout" to=''>Logout</Link>
-                </li>
-            </ul>
-        </nav>
+      <nav className="navbar navbar--nav">
+        <ul className="navbar navbar--list">
+          <li className="navbar navbar--logo">
+            <img className="logo" alt="app logo" />
+          </li>
+          <li className="navbar navbar--profile_picture">
+            <img src={profile_picture_holder} className="" alt="profile" />
+          </li>
+          <li className="navbar navbar--list_item">
+            <img
+              src={dashboard_icon}
+              className="navbar navbar--icon"
+              alt="dashboard page"
+            />
+            <Link to="/Dashboard" className="navbar navbar--text">
+              Dashboard
+            </Link>
+          </li>
+          <li className="navbar navbar--list_item">
+            <img
+              src={profile_icon}
+              className="navbar navbar--icon"
+              alt="profile page"
+            />
+            <Link to="/Profile" className="navbar navbar--text">
+              Profile
+            </Link>
+          </li>
+          <li className="navbar navbar--list_item">
+            <img
+              src={tracker_icon}
+              className="navbar navbar--icon"
+              alt="tracker page"
+            />
+            <Link to="/Tracker" className="navbar navbar--text">
+              Tracker
+            </Link>
+          </li>
+          <li className="navbar--button--div">
+            <button className="navbar navbar--button">Add Task +</button>
+          </li>
+          <li className="navbar navbar--logout">
+            <img
+              src={logout_icon}
+              className="navbar--icon_logout"
+              alt="logout"
+            />
+            <button className="navbar--text_logout" onClick={handleLogout}>
+              Logout
+            </button>
+          </li>
+        </ul>
+      </nav>
     </div>
-    )   
+  );
 };
 
 export { NavBar };
