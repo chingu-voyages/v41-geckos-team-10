31226bb05c71cf4b008dc3d49e472aec47e5797f
Jeremiah Moore <jdmenterprises@gmail.com>
2022-10-27 20:00:00 -0600

    minor changes from main project

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,13 +3,26 @@ import DashWeeklyAppGraph from './DashWeeklyAppGraph';
 import DashJobCards from './DashJobCards';
 import DashGoalItems from './DashGoalItems';
 import DashHeader from './DashHeader';
+import { NavBar } from '../NavBar';
 import './Dashboard.css'
+import {Drawer, ButtonToolbar, Button} from 'rsuite';
+import { useState } from 'react';
 
 const Dashboard = () => {
-
+  const [open, setOpen] = useState(false);
     return(
-        <div className='dashboard '>
-            <DashHeader />
+          <div className='dashboard '>
+  <ButtonToolbar>
+    <Button onClick={() => setOpen(true)}>Open</Button>
+  </ButtonToolbar>
+
+  <Drawer placement="left" open={open} onClose={() => setOpen(false)}>
+    <Drawer.Body>
+    <NavBar />
+    </Drawer.Body>
+  </Drawer> 
+
+    <DashHeader />
             <div className='dashboard_tracker'>
                 <p className='dashboard_tracker_text'>
                     Most Active Goals and Achievments
