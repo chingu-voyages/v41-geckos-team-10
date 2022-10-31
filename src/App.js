a80408fa0fdca869d189f326f60f1a1f5acafe0d
Cody Marshall <codymstuntsx@gmail.com>
2022-10-31 14:19:35 -0700

    remoeved navbar from landing page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { NavBar } from "./components/NavBar";
 import { Outlet } from "react-router-dom";
 import Landing from "./components/Landing";
-import { useState } from 'react';
 import "./App.css";
 
 export default function App() {
 
   return (
     <div >
-      <NavBar /> 
       <Landing />
       <Outlet />
     </div>
