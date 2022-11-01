f7af4390e6a0f99f7403810fd9b8490bc2c88749
Kristi <kristiana.d.ingco@gmail.com>
2022-11-01 16:12:32 -0700

    Extract sortSelection property for use in Tracker

--- src/components/Tracker/SortJobs.jsx
+++ src/components/Tracker/SortJobs.jsx
@@ -1,48 +1,49 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import './SortJobs.css';
+import React from "react";
+import { useEffect } from "react";
+import "./SortJobs.css";
 
 const SortJobs = (props) => {
-    const [sortSelection, setSortSelection] = useState("blank")
-    let jobsList = [...props.jobs];
+  let jobsList = [...props.jobs];
+  let sortSelection = props.sortSelection;
+  let setSortSelection = props.setSortSelection;
 
-    useEffect(() => {
-        // Sorts according to which option was selected.
-        if (sortSelection === "jobTitleAZ") {
-            jobsList.sort((a, b) =>
-                a.title.localeCompare(b.title)
-            )
-        } else if (sortSelection === "companyAZ") {
-            jobsList.sort((a, b) =>
-                a.company.localeCompare(b.company)
-            )
-        } else if (sortSelection === "locationAZ") {
-            jobsList.sort((a, b) =>
-                a.location.localeCompare(b.location)
-            )
-        }
-        props.setData(jobsList)
-
-    }, [sortSelection])
-
-    function updateSortSelection(e) {
-        // Updates the sortSelection state to the selected sort option
-        setSortSelection(e.target.value); 
+  useEffect(() => {
+    // Sorts according to which option was selected.
+    if (sortSelection === "jobTitleAZ") {
+      jobsList.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (sortSelection === "companyAZ") {
+      jobsList.sort((a, b) => a.company.localeCompare(b.company));
+    } else if (sortSelection === "locationAZ") {
+      jobsList.sort((a, b) => a.location.localeCompare(b.location));
+    } else if (sortSelection === "newest") {
+      jobsList.sort((a, b) => a.dateApplied.localeCompare(b.dateApplied));
     }
+    props.setData(jobsList);
+  }, [sortSelection]);
 
-    return (
-        <div className='tracker-page__sort-container'>
-           <label for="sort-jobs">Sort by:</label>
-            <select className="tracker-page__sort-menu" name="sortList" id="sort-jobs" onChange={updateSortSelection}>
-                <option value="newest">Newest first</option>
-                <option value="oldest">Oldest first</option>
-                <option value="jobTitleAZ">Job title</option>
-                <option value="companyAZ">Company</option>
-                <option value="locationAZ">Location</option>
-            </select>
-        </div>
-    )
-    
-}
+  function updateSortSelection(e) {
+    // Updates the sortSelection state to the selected sort option
+    setSortSelection(e.target.value);
+  }
+
+  return (
+    <div className="tracker-page__sort-container">
+      <label for="sort-jobs">Sort by:</label>
+      <select
+        className="tracker-page__sort-menu"
+        name="sortList"
+        id="sort-jobs"
+        onChange={updateSortSelection}
+        value={sortSelection}
+      >
+        <option value="newest">Newest first</option>
+        <option value="oldest">Oldest first</option>
+        <option value="jobTitleAZ">Job title</option>
+        <option value="companyAZ">Company</option>
+        <option value="locationAZ">Location</option>
+      </select>
+    </div>
+  );
+};
 
 export default SortJobs;
