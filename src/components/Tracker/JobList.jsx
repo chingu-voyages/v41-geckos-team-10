5812669edc4b6c59fc83ae11fe870858b734dbee
Cody Marshall <codymstuntsx@gmail.com>
2022-11-02 11:57:29 -0700

    axios call to fetch jobs

--- src/components/Tracker/JobList.jsx
+++ src/components/Tracker/JobList.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import JobCard from './JobCard';
 
-const JobList = ({jobs}) => {
+const JobList = () => {
+    const [jobList, setJobList] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/jobs', {withCredentials: true})
+        .then((res) => {
+            if (res.status === 200) {
+                console.log(res.data);
+                setJobList(res.data);
+            } else {
+                console.log('Error');
+            }
+        })
+    }, []);
     
     return (
         <>
-        {jobs.map((data) =>  
+        {jobList.map((data) =>  
             <JobCard 
                 className="tracker-page__card" 
                 jobDetails={data} 
-                key={data.id}
+                key={data._id}
             /> 
         )} 
         </>
