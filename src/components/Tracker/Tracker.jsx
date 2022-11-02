0ed3bbacb81465eeacaa03b32ecef801e7e3ebb2
Cody Marshall <codymstuntsx@gmail.com>
2022-11-02 12:01:40 -0700

    formatted the page better

--- src/components/Tracker/Tracker.jsx
+++ src/components/Tracker/Tracker.jsx
@@ -1,65 +1,62 @@
-import React from 'react';
-import './Tracker.css';
-import { NavBar } from '../NavBar';
-import SortJobs from './SortJobs';
-import { JOBS } from '../../dummycardata';
-import TrackerFilter from './TrackerFilter'
-import JobList from './JobList'
-import AddJob from '../AddJob/AddJob';
-import { useState } from 'react';
+import React from "react";
+import "./Tracker.css";
+import { NavBar } from "../NavBar";
+import SortJobs from "./SortJobs";
+import { JOBS } from "../../dummycardata";
+import TrackerFilter from "./TrackerFilter";
+import JobList from "./JobList";
+import AddJob from "../AddJob/AddJob";
+import { useState } from "react";
 
 const Tracker = () => {
+  const [data, setData] = useState(JOBS);
+  const [openTrackerDrawer, setOpenTrackerDrawer] = useState("hidden");
 
-    
-    const [ data, setData ] = useState(JOBS)
-    const [openTrackerDrawer, setOpenTrackerDrawer ] = useState({isClosed: true});
-
-    const handleOpenTrackerDrawer = () => {
-        setOpenTrackerDrawer({ isClosed: !openTrackerDrawer.isClosed});
+  const filterHandler = (selected) => {
+    if (selected.toLowerCase() === "all") {
+      setData(JOBS);
+    } else {
+      setData(
+        JOBS.filter(
+          (job) => job.status.toLowerCase() === selected.toLowerCase()
+        )
+      );
     }
+  };
 
-    const filterHandler = (selected) => {
-        if(selected.toLowerCase() === "all") {
-            setData(JOBS)
-        } else {
-            setData(JOBS.filter(job => job.status.toLowerCase() === selected.toLowerCase()))
-        }
-    }
-    
-    const [openTrackerDrawer, setOpenTrackerDrawer ] = useState('hidden');
+  const handleOpenTrackerDrawer = () => {
+    openTrackerDrawer === "hidden"
+      ? setOpenTrackerDrawer("visible")
+      : setOpenTrackerDrawer("hidden");
+  };
 
-    const handleOpenTrackerDrawer = () => {
-       ( openTrackerDrawer === 'hidden') ? setOpenTrackerDrawer('visible') : setOpenTrackerDrawer('hidden')
-    }
+  const style = {
+    visibility: openTrackerDrawer.isClosed ? "hidden" : "visible",
+  };
 
-    const style = {visibility: openTrackerDrawer.isClosed ? 'hidden' : 'visible'};
+  return (
+    <div className="tracker-div">
+      <NavBar />
 
-    return(
-        <div className="tracker-div">
-            <NavBar />
-            
-            <div className='tracker-content'>
-                <TrackerFilter filterHandler={filterHandler}/>
-                <div className='tracker-control'>
-                    <SortJobs 
-                        jobs={data}
-                        setData={setData}
-                    />
-                    <button
-                        className='tracker-button' 
-                        onClick={handleOpenTrackerDrawer}> Create Tracker </button>
-                </div>
-                <div>
-                <JobList jobs={data}/>
-                </div>
-                
-            </div>
+      <div className="tracker-content">
+        <TrackerFilter filterHandler={filterHandler} />
+        <div className="tracker-control">
+          <SortJobs jobs={data} setData={setData} />
+          <button className="tracker-button" onClick={handleOpenTrackerDrawer}>
+            {" "}
+            Create Tracker{" "}
+          </button>
+        </div>
+        <div>
+          <JobList jobs={data} />
+        </div>
+      </div>
 
-            <div className={`tracker_drawer ${openTrackerDrawer}`}>
-                <AddJob handleOpenTrackerDrawer={handleOpenTrackerDrawer}/>
-            </div>
-        </div> 
-    );
+      <div className={`tracker_drawer ${openTrackerDrawer}`}>
+        <AddJob handleOpenTrackerDrawer={handleOpenTrackerDrawer} />
+      </div>
+    </div>
+  );
 };
 
 export default Tracker;
