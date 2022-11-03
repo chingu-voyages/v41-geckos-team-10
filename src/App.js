3a790b192a9e8fc34686133b3d2284a0069fe29b
Cody Marshall <codymstuntsx@gmail.com>
2022-11-03 15:08:03 -0700

    moved routes to app render nav on everypage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
+import "./App.css";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import Landing from "./components/Landing";
-import "./App.css";
+import Tracker from "./components/Tracker/Tracker";
+import Dashboard from "./components/Dashboard/Dashboard";
+import ProfilePage from "./components/ProfilePage/ProfilePage";
+import NavBar from "./components/NavBar";
 
 export default function App() {
+  const location = useLocation();
 
   return (
-    <div>
-      <Landing />
-      <Outlet />
+    <div className="App">
+      {location.pathname === "/" ? null : <NavBar />}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="/tracker" element={<Tracker />} />
+        <Route path="profile-page" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 }
