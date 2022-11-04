21ee4e5725e420ec49614c6cf229851d5e2c8eef
Cody Marshall <codymstuntsx@gmail.com>
2022-11-04 13:03:41 -0700

    commented code for context, added selectjobstate

--- src/components/Tracker/Tracker.jsx
+++ src/components/Tracker/Tracker.jsx
@@ -1,21 +1,58 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import "./Tracker.css";
-import { JOBS } from "../../dummycardata";
 import SortJobs from "./SortJobs";
 import TrackerFilter from "./TrackerFilter";
 import JobList from "./JobList";
 import AddJob from "../AddJob/AddJob";
+import EditJobPanel from "./EditJobPanel";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { displayJobs } from "../../redux/Slices/jobSlice";
-import { useSelector } from "react-redux";
 import IsLoggedIn from "../IsLoggedIn";
 
+/*
+function reducer(state, action) {
+  switch (action.type) {
+    case "EDIT":
+      let newState = state.map((item) => {
+        if (item.id === action.id) {
+          return { ...item, ...action.payload };
+        } else {
+          return item;
+        }
+      });
+      if (action.sortSelection !== "blank") {
+        if (action.sortSelection === "jobTitleAZ") {
+          return newState.sort((a, b) => a.jobtitle.localeCompare(b.jobTitle));
+        } else if (action.sortSelection === "companyAZ") {
+          return newState.sort((a, b) => a.companyName.localeCompare(b.companyName));
+        } else if (action.sortSelection === "locationAZ") {
+          return newState.sort((a, b) => a.companyLocation.localeCompare(b.companyLocation));
+        } else if (action.sortSelection === "newest") {
+          return newState.sort((a, b) =>
+            a.dateApplied.localeCompare(b.dateApplied)
+          );
+        }
+      }
+      return newState;
+    default:
+      return state;
+  }
+}
+*/
+
 const Tracker = () => {
-  const [openTrackerDrawer, setOpenTrackerDrawer] = useState("hidden");
   const user = useSelector((state) => state.user.value);
-  const [data, setData] = useState([]);
+  const jobs = useSelector((state) => state.jobs.value);
+  const [openTrackerDrawer, setOpenTrackerDrawer] = useState("hidden");
+  const [sortSelection, setSortSelection] = useState("blank");
+  const [openEditTrackerDrawer, setOpenEditTrackerDrawer] = useState({
+    isClosed: true,
+  });
+  const [selectedJob, setSelectedJob] = useState({});
+  const [filter, setFilter] = useState("all");
+  const [focusId, setFocusId] = useState(0);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -31,17 +68,55 @@ const Tracker = () => {
       });
   }, []);
 
+  /*
   const filterHandler = (selected) => {
     if (selected.toLowerCase() === "all") {
-      setData(JOBS);
+      if (sortSelection === "jobTitleAZ") {
+        jobs.sort((a, b) => a.jobTitle.localeCompare(b.jobTitle));
+      } else if (sortSelection === "companyAZ") {
+        jobs.sort((a, b) => a.companyName.localeCompare(b.companyName));
+      } else if (sortSelection === "locationAZ") {
+        jobs.sort((a, b) => a.companyLocation.localeCompare(b.companyLocation));
+      } else if (sortSelection === "newest") {
+        jobs.sort((a, b) => a.dateApplied.localeCompare(b.dateApplied));
+      }
+      setJobs(state);
+      setFocusId(0);
     } else {
-      setData(
-        JOBS.filter(
-          (job) => job.status.toLowerCase() === selected.toLowerCase()
-        )
+      const filtered = state.filter(
+        (job) => job.status.toLowerCase() === selected.toLowerCase()
+      );
+      if (sortSelection === "jobTitleAZ") {
+        filtered.sort((a, b) => a.jobTitle.localeCompare(b.jobTitle));
+      } else if (sortSelection === "companyAZ") {
+        filtered.sort((a, b) => a.companyName.localeCompare(b.companyName));
+      } else if (sortSelection === "locationAZ") {
+        filtered.sort((a, b) => a.companyLocation.localeCompare(b.companyLocation));
+      } else if (sortSelection === "newest") {
+        filtered.sort((a, b) => a.dateApplied.localeCompare(b.dateApplied));
+      }
+      if (filtered.length !== 0) {
+        setJobs(filtered);
+        setFocusId(0);
+      } else {
+        setJobs([]);
+      }
+    }
+
+    setFilter(selected.toLowerCase());
+  }; */
+
+  /*
+  useEffect(() => {
+    if (filter === "all") {
+      setFilteredJobs(state);
+    } else {
+      setJobs(
+        state.filter((job) => job.trackerStatus.toLowerCase() === filter.toLowerCase())
       );
     }
-  };
+  }, [state]);
+  */
 
   const handleOpenTrackerDrawer = () => {
     openTrackerDrawer === "hidden"
@@ -49,32 +124,61 @@ const Tracker = () => {
       : setOpenTrackerDrawer("hidden");
   };
 
-  const style = {
-    visibility: openTrackerDrawer.isClosed ? "hidden" : "visible",
+  const handleOpenEditTrackerDrawer = () => {
+    setOpenEditTrackerDrawer({ isClosed: !openEditTrackerDrawer.isClosed });
+  };
+
+  const editStyle = {
+    visibility: openEditTrackerDrawer.isClosed ? "hidden" : "visible",
+  };
+
+  /*const handleToggle = (id) => {
+    setFocusId(id);
+  };*/
+
+  const handleClick = (job) => {
+    setSelectedJob(job); 
+    //handleToggle(idx);
+    handleOpenEditTrackerDrawer();
   };
 
   if (user.isLoggedIn) {
     return (
-      <div className="tracker-div">
-        <div className="tracker-content">
-          <TrackerFilter filterHandler={filterHandler} />
-          <div className="tracker-control">
-            <SortJobs setData={setData} />
-            <button
-              className="tracker-button"
-              onClick={handleOpenTrackerDrawer}
-            >
-              {" "}
-              Create Tracker{" "}
-            </button>
+      <div className="tracker-page">
+        <div className="tracker-div">
+          <div className="tracker-content">
+            <TrackerFilter
+              /* filterHandler={filterHandler} */
+              openEditTrackerDrawer={openEditTrackerDrawer}
+              setOpenEditTrackerDrawer={setOpenEditTrackerDrawer}
+            />
+            <div className="tracker-control">
+              <SortJobs
+                jobs={jobs}
+                sortSelection={sortSelection}
+                setSortSelection={setSortSelection}
+              />
+              <button
+                className="tracker-button"
+                onClick={handleOpenTrackerDrawer}
+              >
+                Create Tracker
+              </button>
+            </div>
+            <JobList jobs={jobs} handleClick={handleClick}/>
           </div>
-          <div>
-            <JobList />
+          <div className={`tracker_drawer ${openTrackerDrawer}`}>
+            <AddJob handleOpenTrackerDrawer={handleOpenTrackerDrawer} />
           </div>
-        </div>
-
-        <div className={`tracker_drawer ${openTrackerDrawer}`}>
-          <AddJob handleOpenTrackerDrawer={handleOpenTrackerDrawer} />
+          <div style={editStyle}>
+            <EditJobPanel
+              dispatch={dispatch}
+              focusId={focusId}
+              sortSelection={sortSelection}
+              handleOpenTrackerDrawer={handleOpenEditTrackerDrawer}
+              selectedJob={selectedJob}
+            />
+          </div>
         </div>
       </div>
     );
