7b69ab8ff2d29f55b096404b38c9be73cc24043e
MoxElliot <eric0952@gmail.com>
2022-11-12 11:35:13 -0800

    removed console.log, profilePic
    Removed console.log and unused profilePic state from DashHeader.jsx

--- src/components/Dashboard/DashHeader.jsx
+++ src/components/Dashboard/DashHeader.jsx
@@ -6,12 +6,9 @@ import './DashHeader.css';
 
 const DashHeader = () => {
 
-    const [profilePic, setProfilePic] = useState("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Uwo-kkvTH_pEjCu0O4z7oQAAAA%26pid%3DApi&f=1&ipt=05ed7a42f68098e6fac9bfc7fd61e9576b8082bdb45f0bc2adacca5b37256e03&ipo=images");
-
     const userFirstName = useSelector((state) => state.profile.value.firstName);
     const userLastName = useSelector((state) => state.profile.value.lastName);
 
-    console.log("First, Last", (userFirstName +" "+ userLastName))
 
     return (
         <div className='dashboard_user-profile'>
